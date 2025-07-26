501ac5735ade7f81e16e96cc4a025168e4e240d1
bhavanapatidar <bhavanapatidar2708@gmail.com>
2025-07-26 12:38:19 +0530

    NFT Collection with Metadata & safeMint (No OpenZeppelin)

--- Contract_Conqueror_Series/ERC721standards.sol
+++ Contract_Conqueror_Series/ERC721standards.sol
@@ -1,85 +1,84 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-/// @title BasicERC721
-/// @notice Basic ERC721-compliant NFT implementation (without OpenZeppelin)
-contract BasicERC721 {
+/// @title MetadataNFT
+/// @notice Basic NFT collection with metadata and safeMint function
+contract MetadataNFT {
     string public name;
     string public symbol;
+    string private baseURI;
     uint256 public totalSupply;
+    address public owner;
 
-    // Mapping from token ID to owner
     mapping(uint256 => address) private _owners;
-
-    // Mapping from owner to number of owned tokens
     mapping(address => uint256) private _balances;
+    mapping(uint256 => string) private _tokenURIs;
 
-    // Mapping from token ID to approved address
-    mapping(uint256 => address) private _tokenApprovals;
-
-    // Events per ERC721 standard
     event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
-    event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
 
-    constructor(string memory _name, string memory _symbol) {
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not contract owner");
+        _;
+    }
+
+    constructor(string memory _name, string memory _symbol, string memory _baseURI) {
         name = _name;
         symbol = _symbol;
+        baseURI = _baseURI;
+        owner = msg.sender;
+    }
+
+    /// @notice Returns balance of a specific owner
+    function balanceOf(address _owner) public view returns (uint256) {
+        require(_owner != address(0), "Zero address");
+        return _balances[_owner];
     }
 
     /// @notice Returns the owner of a token
     function ownerOf(uint256 tokenId) public view returns (address) {
-        address owner = _owners[tokenId];
-        require(owner != address(0), "Token does not exist");
-        return owner;
+        address _owner = _owners[tokenId];
+        require(_owner != address(0), "Token doesn't exist");
+        return _owner;
     }
 
-    /// @notice Returns the number of tokens owned by `owner`
-    function balanceOf(address owner) public view returns (uint256) {
-        require(owner != address(0), "Zero address not allowed");
-        return _balances[owner];
+    /// @notice Returns metadata URI for a token
+    function tokenURI(uint256 tokenId) public view returns (string memory) {
+        require(_owners[tokenId] != address(0), "Token doesn't exist");
+        return string(abi.encodePacked(baseURI, uint2str(tokenId), ".json"));
     }
 
-    /// @notice Approve another address to transfer the given token ID
-    function approve(address to, uint256 tokenId) public {
-        address owner = _owners[tokenId];
-        require(msg.sender == owner, "Not token owner");
-        require(to != owner, "Approval to current owner");
+    /// @notice Internal mint function (only callable by owner)
+    function safeMint(address to) public onlyOwner {
+        require(to != address(0), "Mint to zero address");
 
-        _tokenApprovals[tokenId] = to;
-        emit Approval(owner, to, tokenId);
-    }
-
-    /// @notice Get approved address for a token ID
-    function getApproved(uint256 tokenId) public view returns (address) {
-        return _tokenApprovals[tokenId];
-    }
-
-    /// @notice Transfers a token from one address to another
-    function transferFrom(address from, address to, uint256 tokenId) public {
-        address owner = _owners[tokenId];
-        require(owner == from, "Not token owner");
-        require(msg.sender == owner || msg.sender == _tokenApprovals[tokenId], "Not approved");
-
-        require(to != address(0), "Cannot transfer to zero address");
-
-        _balances[from] -= 1;
-        _balances[to] += 1;
-
-        _owners[tokenId] = to;
-        delete _tokenApprovals[tokenId];
-
-        emit Transfer(from, to, tokenId);
-    }
-
-    /// @notice Mints a new NFT token
-    function mint(address to, uint256 tokenId) public {
-        require(to != address(0), "Cannot mint to zero address");
-        require(_owners[tokenId] == address(0), "Token already exists");
-
+        uint256 tokenId = totalSupply + 1;
         _balances[to] += 1;
         _owners[tokenId] = to;
-        totalSupply += 1;
+        totalSupply = tokenId;
 
         emit Transfer(address(0), to, tokenId);
+    }
+
+    /// @notice Change base URI
+    function setBaseURI(string memory _base) public onlyOwner {
+        baseURI = _base;
+    }
+
+    /// @dev Converts uint to string (for tokenURI)
+    function uint2str(uint256 _i) internal pure returns (string memory str) {
+        if (_i == 0) return "0";
+        uint256 temp = _i;
+        uint256 digits;
+        while (temp != 0) {
+            digits++;
+            temp /= 10;
+        }
+        bytes memory bStr = new bytes(digits);
+        while (_i != 0) {
+            digits -= 1;
+            bStr[digits] = bytes1(uint8(48 + _i % 10));
+            _i /= 10;
+        }
+        str = string(bStr);
     }
 }
